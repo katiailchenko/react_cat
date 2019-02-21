b4e84daff6d71fcc69108ff5656976c063f73f30
katia_ilchenko <k.ilchenko@8ways.ch>
2019-02-21 23:24:49 +0200

    created ProductList

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,2 +1,19 @@
 import './ProductItem.css';
-import React, { Component } from 'react';
+import React from 'react';
+
+const ProductItem = ({name, imageUrl, snippet}) => (
+    <div className='mobile-card'>
+        <div className='mobile-card__img'>
+            <a href='#'>
+                <img src={require(`../../Data/${imageUrl}`)} alt={name}/>
+            </a>
+        </div>
+        <div className='mobile-card__text'>
+            <a href='#' className='mobile-card__title'>{name}</a>
+            <p className='mobile-card__descr'>{snippet}</p>
+        </div>
+    </div>
+)
+
+
+export default ProductItem;
